fedad9cc6688c87d79ad85a2c2e618932a683c9e
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-06-13 12:29:32 +0900

    fixed

--- initializer.sh
+++ initializer.sh
@@ -1,13 +1,12 @@
 #! /bin/sh
 
-# Install vim plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-# Install OhMyZsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cat ~/.zshrc.pre-oh-my-zsh >> ~/.zshrc
 # Create symbolic links
 ln -s `pwd`/.config/nvim ~/.config/nvim
 ln -s `pwd`/.zshrc ~/.zshrc
 
-
+# Install vim plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# Install OhMyZsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
