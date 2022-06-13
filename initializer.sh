f9d6446d5218a8285ea0a5d28b2e4123f17d4737
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-06-13 12:22:16 +0900

    fixed bug

--- initializer.sh
+++ initializer.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 
+# Install vim plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # Install OhMyZsh
-curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cat ~/.zshrc.pre-oh-my-zsh >> ~/.zshrc
 # Create symbolic links
 ln -s `pwd`/.config/nvim ~/.config/nvim
