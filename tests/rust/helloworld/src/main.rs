25349380daaab8a070d38b3bbeb37f6631e600ec
kackey0-1 <50774322+kackey0-1@users.noreply.github.com>
2022-10-11 09:44:20 +0900

    updated settings (#10)
    * updated settings
    
    * fixed install version of coc.nvim

--- tests/rust/helloworld/src/main.rs
+++ tests/rust/helloworld/src/main.rs
@@ -1,29 +1,46 @@
-use std::error::Error;
-use std::io::{Read, Write};
-use std::net::TcpListener;
-use std::{env, str, thread};
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    let addr = &args[1];
-    echo_server(addr)?;
-    Ok(())
+fn main() {
+    let name = "dorayakikun";
+    println!("{}", name);
+    vec_sample();
+    if_sample();
+    loop_sample();
+    for_sample();
 }
 
-fn echo_server(address: &str) -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind(address)?;
-    loop {
-        let (mut stream, _) = listener.accept()?;
-        thread::spawn(move || {
-            let mut buffer = [0u8; 1024];
-            loop {
-                let nbytes = stream.read(&mut buffer).unwrap();
-                if nbytes == 0 {
-                    return;
-                }
-                print!("{}", str::from_utf8(&buffer[..nbytes]).unwrap());
-                stream.write_all(&buffer[..nbytes]).unwrap();
-            }
-        });
+fn vec_sample() {
+    let animals = vec!["ookami", "shirokuma", "fukuroh"];
+    for i in 0..animals.len() {
+        println!("animals[{}] = {}", i, animals[i]);
     }
 }
+
+fn if_sample() {
+    let age = 19;
+    if age < 20 {
+        println!("age < 20");
+    } else {
+        println!("age >= 20");
+    }
+}
+
+fn loop_sample() {
+    let mut count = 0;
+    loop {
+        println!("loop: count is {}", count);
+        count += 1;
+        if count >= 10 {
+            break;
+        }
+    }
+}
+
+fn for_sample() {
+    for i in 0..10 {
+        println!("for: i is {}", i);
+    }
+}
+
+// #[wasm_bindgen]
+// pub fn greet(name: &str) {
+//     alert(&format!("Hello, {}", name));
+// }
