aaf363a1b445d6d7e1c2a31c84f50e431f2a782f
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-10-17 23:02:15 +0900

    added helloworld

---
+++ tests/clangpp/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// 名前空間
+using namespace std;
+
+int main() {
+    std::cout << "HelloWorld." << std::endl;
+    // cout << "Hello World!!" << endl;
+    int a;
+    // キーボードから入力した数値をそのまま出力
+    cin >> a;
+    cout << "a=" << a << endl;
+    return 0;
+}
+
