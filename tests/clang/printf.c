1e1bf5e6b057433116645c8d7f9bed5e749b9bba
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-10-17 22:45:10 +0900

    added basic of clang

--- tests/clang/printf.c
+++ tests/clang/printf.c
@@ -15,9 +15,9 @@ void main() {
     printf("8進数  result with 1000: %02o\n", 1000);
     printf("8進数  result with 1000: %06o\n", 1000);
     printf("16進数 result with 1000: %x\n", 1000);
-    printf("############################################################\n");
 
-    /* function */
+    /* add sub / */
+    printf("############################################################\n");
     int a;
     int b = 3;
     int add, sub;
@@ -31,5 +31,25 @@ void main() {
     printf("%dと%dの平均値: %f\n", a, b, avg);
     printf("%dと%dの平均値: %.2f\n", a, b, avg);
 
+    /* cast data type */
+    printf("############################################################\n");
+    int i1,i2,j1,j2;
+    double d1,d2,e1,e2;
+    //  j1,j2に値を代入
+    j1 = 3;
+    j2 = 3;
+    //  d1,d2に値を代入。
+    d1 = 1.23;
+    d2 = 1.23;
+    //  i1,i2にd1,d2の値を代入
+    i1 = d1; //  普通に代入
+    i2 = (int)d2; //  キャストして代入
+    // e1,e2にj1,j2の値を代入
+    e1 = j1; //  普通に代入
+    e2 = (double)j2; //  キャストして代入
+    printf("d1 = %f d2 = %f\n",d1,d2);
+    printf("i1 = %d i2 = %d\n",i1,i2);
+    printf("j1 = %d j2 = %d\n",j1,j2);
+    printf("e1 = %f e2 = %f\n",e1,e2);
 }
 
