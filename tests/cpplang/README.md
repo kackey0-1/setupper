913af9e5b8e57f2e7eaf7ab3991566611805b761
kackey0-1 <50774322+kackey0-1@users.noreply.github.com>
2022-10-18 23:00:22 +0900

    finished until day 2 (#14)

--- tests/cpplang/README.md
+++ tests/cpplang/README.md
@@ -8,8 +8,17 @@ g++ main.cpp
 
 ファイルへの読み書きと、コンソールからの入力・出力の処理がほとんど同じような記述で行うことが出来る
 
+https://mermaid-js.github.io/mermaid/#/
 
+```
+cout -> stream <-> file
+cin  <- stream <-> console
+```
 
-
+|identification|meaning|
+|---|---|
+|public|すべての範囲から呼び出し・読み出し可能|
+|private|同一クラスまたは同一インスタンス内でのみアクセス可能|
+|protected|同一クラスまたは同一インスタンスおよび、サブクラスおよびそのインスタンス内でのみアクセス可能|
 
 
--- tests/cpplang/main.cpp
+++ tests/cpplang/main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <iterator>
+#include "sample.h"
 
 // 名前空間
 using namespace std;
 
 int main() {
-    std::cout << "HelloWorld." << std::endl;
-    // cout << "Hello World!!" << endl;
-    int a;
-    // キーボードから入力した数値をそのまま出力
-    cin >> a;
-    cout << "a=" << a << endl;
+//     std::cout << "HelloWorld." << std::endl;
+//     cout << "Hello World!!" << endl;
+//     int a;
+//     キーボードから入力した数値をそのまま出力
+//     cin >> a;
+//     cout << "a=" << a << endl;
+
+//     string s, t;
+//     t = "input string value is \"";
+//     cout << "input string: ";
+//     cin >> s;
+//     cout << t+s << "\"." << endl;
+    CSample obj;
+    int num;
+    cout << "INPUT NUMBER: " << endl;
+    cin >> num;
+    obj.set(num);
+    cout
+        << "input number: "
+        << obj.get()
+        << endl;
     return 0;
 }
 
